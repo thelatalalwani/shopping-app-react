28d2d2eda3e638d6a2c0531e43eb6c4e5cd6ca54
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-16 23:07:29 +0530

    feat: implement Home component to display products

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,7 @@
-import { useEffect, useState } from "react";
-import { getProducts } from "./services/productService";
+import Home from "./pages/Home";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    loadProducts();
-  }, []);
-
-  async function loadProducts() {
-    try {
-      const data = await getProducts();
-      console.log("Products:", data);
-      setProducts(data);
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
-
-  return (
-    <div>
-      <h1>Shopping App</h1>
-
-      {products.map((product) => (
-        <div
-          key={product.id}
-          style={{
-            border: "1px solid #ccc",
-            margin: "10px",
-            padding: "10px",
-          }}
-        >
-          <h3>{product.name}</h3>
-          <p>{product.description}</p>
-          <p>₹ {product.price}</p>
-          <p>Stock : {product.stock}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
