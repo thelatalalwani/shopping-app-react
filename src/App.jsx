870f8a1b53ea84c41ff549e1666ed0fa9a9086c3
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-18 00:45:36 +0530

    feat: pass cartItems prop to Cart component for displaying items in the shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
 
         <Route path="/products/:id" element={<ProductDetails />} />
 
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart cartItems={cartItems} />} />
       </Routes>
     </>
   );
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,26 @@
-function Cart() {
+function Cart({ cartItems }) {
   return (
     <div>
       <h1>Shopping Cart</h1>
+
+      {cartItems.map((item) => (
+        <div
+          key={item.product.id}
+          style={{
+            border: "1px solid gray",
+            margin: "10px",
+            padding: "10px",
+          }}
+        >
+          <h3>{item.product.name}</h3>
+
+          <p>Price: ₹{item.product.price}</p>
+
+          <p>Quantity: {item.quantity}</p>
+
+          <p>Subtotal: ₹{item.product.price * item.quantity}</p>
+        </div>
+      ))}
     </div>
   );
 }
