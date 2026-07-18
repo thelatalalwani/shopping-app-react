74be4578dab6e5e801c37a6550f68b2d3bc1a494
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-18 14:17:10 +0530

    feat: implement name input field in Checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function Checkout() {
+  const [name, setName] = useState("");
+
   return (
     <div>
       <h1>Checkout</h1>
+
+      <div>
+        <label>Name</label>
+
+        <br />
+
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
     </div>
   );
 }
