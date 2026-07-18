a9f4e9ac351d2d0618f1477bd0ebc8ee41922bc6
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-18 14:25:29 +0530

    feat: add email input field in Checkout component

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function Checkout() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
     <div>
@@ -16,6 +17,18 @@ function Checkout() {
           type="text"
           value={name}
           onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <label>Email</label>
+
+        <br />
+
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
     </div>
