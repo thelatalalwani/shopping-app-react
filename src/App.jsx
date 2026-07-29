e5a496535016f3caa1f2ce9d3cf2fef3e8753349
Lata Lalwani <297639380+thelatalalwani@users.noreply.github.com>
2026-07-29 13:23:32 +0530

    implemented Lazy loaded routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,164 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import {
+  lazy,
+  Suspense,
+} from "react";
+
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import Layout from "./components/layout/Layout";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminRoute from "./routes/AdminRoute";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AdminProducts from "./pages/admin/AdminProducts";
-import AddProduct from "./pages/admin/AddProduct";
-import EditProduct from "./pages/admin/EditProduct";
+
+const Home = lazy(
+  () => import("./pages/Home"),
+);
+
+const ProductDetails = lazy(
+  () => import("./pages/ProductDetails"),
+);
+
+const Login = lazy(
+  () => import("./pages/Login"),
+);
+
+const Register = lazy(
+  () => import("./pages/Register"),
+);
+
+const Cart = lazy(
+  () => import("./pages/Cart"),
+);
+
+const Checkout = lazy(
+  () => import("./pages/Checkout"),
+);
+
+const Orders = lazy(
+  () => import("./pages/Orders"),
+);
+
+const AdminDashboard = lazy(
+  () =>
+    import(
+      "./pages/admin/AdminDashboard"
+    ),
+);
+
+const AdminProducts = lazy(
+  () =>
+    import(
+      "./pages/admin/AdminProducts"
+    ),
+);
+
+const AddProduct = lazy(
+  () =>
+    import(
+      "./pages/admin/AddProduct"
+    ),
+);
+
+const EditProduct = lazy(
+  () =>
+    import(
+      "./pages/admin/EditProduct"
+    ),
+);
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <Suspense
+        fallback={
+          <p>Loading page...</p>
+        }
+      >
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
 
-        <Route path="/products/:id" element={<ProductDetails />} />
+          <Route
+            path="/products/:id"
+            element={
+              <ProductDetails />
+            }
+          />
 
-        <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/login"
+            element={<Login />}
+          />
 
-        <Route path="/login" element={<Login />} />
+          <Route
+            path="/register"
+            element={<Register />}
+          />
 
-        <Route path="/register" element={<Register />} />
+          <Route
+            path="/cart"
+            element={<Cart />}
+          />
 
-        <Route
-          path="/checkout"
-          element={
-            <ProtectedRoute>
-              <Checkout />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <AdminRoute>
-              <AdminDashboard />
-            </AdminRoute>
-          }
-        />
-        <Route
-          path="/admin/products"
-          element={
-            <AdminRoute>
-              <AdminProducts />
-            </AdminRoute>
-          }
-        />
+          <Route
+            path="/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/admin/products/add"
-          element={
-            <AdminRoute>
-              <AddProduct />
-            </AdminRoute>
-          }
-        />
+          <Route
+            path="/orders"
+            element={
+              <ProtectedRoute>
+                <Orders />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/admin/products/edit/:id"
-          element={
-            <AdminRoute>
-              <EditProduct />
-            </AdminRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path="/admin"
+            element={
+              <AdminRoute>
+                <AdminDashboard />
+              </AdminRoute>
+            }
+          />
+
+          <Route
+            path="/admin/products"
+            element={
+              <AdminRoute>
+                <AdminProducts />
+              </AdminRoute>
+            }
+          />
+
+          <Route
+            path="/admin/products/add"
+            element={
+              <AdminRoute>
+                <AddProduct />
+              </AdminRoute>
+            }
+          />
+
+          <Route
+            path="/admin/products/edit/:id"
+            element={
+              <AdminRoute>
+                <EditProduct />
+              </AdminRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
